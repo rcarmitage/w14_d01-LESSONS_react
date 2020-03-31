9d987a9e22ed42415c78dc424733d4056c3bfe5b
Rob Armitage <robert.c.armitage@gmail.com>
2020-03-31 23:59:33 +0100

    add some FilmList code

---
+++ films/src/components/FilmList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FilmList = ({data}) => {
+  const FilmNodes = data.map(film => {
+    return (
+      <Film title={film.title} key={film.id}>
+        {film.title}
+      </Film>
+    );
+  });
+
+  return 
+}
