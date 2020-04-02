2dbbd4086768aacbb4eb85686802fe6e96e72c5d
Rob Armitage <robert.c.armitage@gmail.com>
2020-04-02 23:47:17 +0100

    FilmLink renders, link doesn't work

--- films/src/components/FilmLink.js
+++ films/src/components/FilmLink.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 
-const FilmLink extends Component {
-  <div>
-    <a url="https://www.imdb.com/calendar/?region=gb">View more upcoming releases >></a>
-  </div>
+const FilmLink = ({link}) => {
+  return (
+    <div>
+      <a url="https://www.imdb.com/calendar/?region=gb">View more upcoming releases >></a>
+    </div>
+  )
 }
 
 export default FilmLink;
